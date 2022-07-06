e8c39e42bad113fd88fa428ba36ee164b689c112
Divu2000 <kaurdivneet2000@gmail.com>
2022-07-06 06:30:30 +0000

    init

---
+++ Migrations/20220628050507_db_v1.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace CalculateNetworth.Migrations
+{
+    public partial class db_v1 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
